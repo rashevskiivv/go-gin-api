a1a068e1e5a4e9368e9fd7bc7969604da781b6a5
e1ko0o <slava121231@mail.ru>
2023-02-19 14:02:51 +0300

    fix init mongo script, docker compose version, mongo healthcheck

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,14 +1,25 @@
-set -e
+mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
 
-mongosh <<EOF
-use $MONGO_INITDB_DATABASE
-
-db.createUser({
-  user: '$MONGO_INITDB_ROOT_USERNAME',
-  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
-  roles: [{
-    role: 'readWrite',
-    db: '$MONGO_INITDB_DATABASE'
-  }]
-})
+    var user = '$MONGO_INITDB_ROOT_USERNAME';
+    var passwd = '$MONGO_INITDB_ROOT_PASSWORD';
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOF
+
+#set -e
+#
+#mongosh <<EOF
+#use $MONGO_INITDB_DATABASE
+#
+#db.createUser({
+#  user: '$MONGO_INITDB_ROOT_USERNAME',
+#  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+#  roles: [{
+#    role: 'readWrite',
+#    db: '$MONGO_INITDB_DATABASE'
+#  }]
+#})
+#EOF
